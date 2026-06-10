135eed8a2794b723a8b49676cfedf1d314000ba6
tvbyte <sk01677603434@gmail.com>
2026-06-10 22:26:35 +0600

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,136 @@
+var MSDATA=JSON.stringify([  
+  
+{
+"id":"rajdhani",
+"name":"Rajdhani TV",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/raj.jpg",
+"url":"https://stream.shariarsuvo.com/hls5/rajdhanicable.m3u8"
+},
+{
+"id":"AAJ_Entertainment",
+"name":"AAJ Entertainment",
+"category":"Indian",
+"logo":"https://fastiptv.page.gd/icon/aaj_entertainment.png",
+"url":"https://ml-pull-dvc-myco.io:2096/AAJ_ENTERTAINMENT/index.m3u8"
+},
+{
+"id":"animal_planet_hd",
+"name":"Animal Planet HD",
+"category":"Infotainment",
+"logo":"https://fastiptv.page.gd/icon/animal_planet_hd.png",
+"url":"https://ottbanglaplatform.com/tv/toffee/live.php?id=animal_planet_hd&e=.m3u8"
+},
+{
+"id":"asports",
+"name":"A Sports",
+"category":"Sports",
+"logo":"https://fastiptv.page.gd/icon/a-sports.jpg",
+"url":"https://tvsen6.aynaott.com/asports/index.m3u8"
+},
+{
+"id":"atnbangla",
+"name":"ATN Bangla",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/atn-bangla1.jpg",
+"url":"https://tvsen5.aynaott.com/atnbangla/index.m3u8"
+},
+{
+"id":"atnnews",
+"name":"ATN News",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/atn_news_bd.png",
+"url":"https://owrcovcrpy.gpcdn.net/bpk-tv/1706/output/index.m3u8"
+},
+{
+"id":"ayna",
+"name":"Ayna",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/ayna.jpg",
+"url":"https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/ayna.stream/index.m3u8"
+},
+{
+"id":"banglavision",
+"name":"Banglavision",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/banglavision.webp",
+"url":"https://owrcovcrpy.gpcdn.net/bpk-tv/1715/output/index.m3u8"
+},
+{
+"id":"BengaliBeats",
+"name":"Bengali Beats",
+"category":"Music",
+"logo":"https://fastiptv.page.gd/icon/Bengali-Beats.png",
+"url":"https://live20.bozztv.com/giatvplayout7/giatv-209587/tracks-v1a1/mono.ts.m3u8"
+},
+{
+"id":"Bein Sports 1",
+"name":"Bein Sports 1 FIFA",
+"category":"Sports",
+"logo":"https://fastiptv.page.gd/icon/Bein Sports 1.jpg",
+"url":"https://andro.226503.xyz/checklist/androstreamlivebs1.m3u8"
+},
+{
+"id":"Bein Sports 2",
+"name":"Bein Sports 2 FIFA",
+"category":"Sports",
+"logo":"https://fastiptv.page.gd/icon/Bein Sports 2.jpg",
+"url":"https://andro.226503.xyz/checklist/androstreamlivebs2.m3u8"
+},
+{
+"id":"Bein Sports 3",
+"name":"Bein Sports 3 FIFA",
+"category":"Sports",
+"logo":"https://fastiptv.page.gd/icon/Bein Sports 3.jpg",
+"url":"https://andro.226503.xyz/checklist/androstreamlivebs3.m3u8"
+},
+{
+"id":"Bein Sports 4",
+"name":"Bein Sports 4 FIFA",
+"category":"Sports",
+"logo":"https://fastiptv.page.gd/icon/Bein Sports 4.jpg",
+"url":"https://andro.226503.xyz/checklist/androstreamlivebs4.m3u8"
+},
+{
+"id":"Bein Sports Xtra",
+"name":"Bein Sports Xtra FIFA",
+"category":"Sports",
+"logo":"https://fastiptv.page.gd/icon/bein_sport_xtra.jpg",
+"url":"https://bein-esp-xumo.amagi.tv/playlistR1080p.m3u8"
+},
+{
+"id":"BBC_Earth",
+"name":"BBC Earth HD",
+"category":"Infotainment",
+"logo":"https://fastiptv.page.gd/icon/BBC_Earth.png",
+"url":"https://d3u3pfhhvuad9k.cloudfront.net/playlist/amg00793-bbcstudios-bbcearta-lgus/playlist.m3u8"
+},
+{
+"id":"BoishakhiTV",
+"name":"Boishakhi TV",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/BoishakhiTV.jpg",
+"url":"https://tvsen6.aynaott.com/boishakhitv/index.m3u8"
+},
+{
+"id":"Bijoy_TV",
+"name":"Bijoy TV",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/Bijoy_TV.jpg",
+"url":"https://tvsen6.aynaott.com/bijoytv/index.m3u8"
+},
+{
+"id":"btvnational",
+"name":"BTV National FIFA",
+"category":"BANGLA",
+"logo":"https://fastiptv.page.gd/icon/btv.png",
+"url":"https://owrcovcrpy.gpcdn.net/bpk-tv/1709/output/index.m3u8"
+},
+{
+"id":"b4u",
+"name":"B4U Music",
+"category":"Music",
+"logo":"https://fastiptv.page.gd/icon/B4-U-Music.png",
+"url":"https://ottbanglaplatform.com/tv/toffee/live.php?id=b4u_music&e=.m3u8"
+},
+
